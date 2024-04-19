7ca513916228798a19149c998c32adf1a7922689
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-04-19 12:00:16 +0300

    Running particles

--- scenes/dino.gd
+++ scenes/dino.gd
@@ -2,14 +2,25 @@ extends CharacterBody2D
 
 const GRAVITY : int = 2800
 const JUMP_SPEED : int = -1200
-	
+
+# Particle variables
+var running_part = false
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta;
 	var is_up := Input.is_action_pressed("game_up") and not Input.is_action_pressed("game_down");
 	var is_down := Input.is_action_pressed("game_down") and not Input.is_action_pressed("game_up");
 	
+	if not get_parent().game_running:
+		$AnimatedSprite2D/GrassPart.emitting = false
+	else:
+		$AnimatedSprite2D/GrassPart.emitting = running_part
+	
 	if is_on_floor():
+		running_part = true
+		
 		if not get_parent().game_running:
 			$AnimatedSprite2D.play("idle");
 		else:
@@ -24,6 +35,8 @@ func _physics_process(delta):
 			else:
 				$AnimatedSprite2D.play("run");
 	else:
+		running_part = false
+		
 		if is_down:
 			velocity.y = -JUMP_SPEED * 1.5;
 		$AnimatedSprite2D.play("jump");
