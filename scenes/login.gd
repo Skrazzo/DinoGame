a32fc59625716e99af328f79878f26902cc2307a
VienkarsiAleksis <ipb21.a.osins@vtdt.edu.lv>
2024-05-14 11:22:23 +0300

    fghfg

---
+++ scenes/login.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_register_pressed():
+	get_tree().change_scene_to_file("res://scenes/register.tscn")
---
+++ scenes/register.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_login_pressed():
+	get_tree().change_scene_to_file("res://scenes/login.tscn")
